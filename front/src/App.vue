45ae7700688b75df7832c6d8e4f8626900787e53
androktg <androktg@gmail.com>
2026-08-03 15:12:07 +0900

    [#4] fix: 전역 리셋 제거 및 workationRouter 등록

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,11 +5,3 @@
 </template>
 
 <script setup></script>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import accountRouter from './accountRouter';
+import workationRouter from './workationRouter';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [...accountRouter],
+  routes: [...accountRouter, ...workationRouter],
 });
 
 export default router;
