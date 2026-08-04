23ffa9dbea5761db068ad8ecf6be6db36a2b8c7f
jeonggilAhn <jeonggilahn@gmail.com>
2026-08-04 13:57:59 +0900

    resolve conflict

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -3,6 +3,7 @@ import accountRouter from './accountRouter';
 import cardRouter from './cardRouter';
 import walletRouter from './walletRouter';
 import transactionRouter from './transactionRouter';
+import workationRouter from './workationRouter';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +12,7 @@ const router = createRouter({
     ...cardRouter,
     ...walletRouter,
     ...transactionRouter,
+    ...workationRouter,
   ],
 });
 
